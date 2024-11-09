f07beb6c36bdabe7743cec6c7d5d0a72022c7dd8
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-11-09 03:03:56 -0600

    Header + Banner components

---
+++ src/components/header/Banner.jsx
@@ -0,0 +1,10 @@
+function Banner() {
+  return (
+        <div className="bg-yellow-500 h-25 flex items-center justify-center">
+            <h1 className="text-black text-center text-6xl font-latin_modern_roman_caps10Ob pt-2">Γ</h1>
+            <h1 className="text-black text-center text-6xl font-latin_modern_roman_caps10Ob">-sense</h1>
+        </div>
+  );
+}
+
+export default Banner;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,35 @@
-import presetUno from '@unocss/preset-uno'
-import { defineConfig } from 'unocss'
+import React from 'react'; 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image';
+// import logo from '../../assets/3d-rendering-caution-danger-symbol.png';
+import logo from '../../assets/grunge-radioactive-sign.png';
+import Row from 'react-bootstrap/Row';
 
-export default defineConfig({
-  presets: [
-    presetUno(),
-  ],
-  include: [
-    // Scan your source files for UnoCSS classes and directives
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-    // Include your global.css file
-    './src/styles/global.css',
-  ],
-  theme: {
-    fontFamily: {
-      'latin_modern_roman_caps10Ob': ['latin_modern_roman_caps10Ob', 'serif'],
-    },
-  },
-})
+function Header() {
+  return (
+    <Navbar collapseOnSelect expand="sm" className="shadow-none bg-transparent position-absolute top-0 start-5 z-10">
+      <Container>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+            <Image
+                src={logo}
+                alt="Γ-sense logo"
+                width={50}
+                height={50}
+            />
+            {/* <span className="ms-2">Γ-sense</span> */}
+        </Navbar.Brand>
+        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#graph">Graph</Nav.Link>
+          </Nav>
+        </Navbar.Collapse> */}
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header
