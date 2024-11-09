f26c1c1bf876b07939964ee5d190ef475a8a4bfa
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-11-09 03:03:39 -0600

    Update packages, add UnoCSS !

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import presetUno from '@unocss/preset-uno'
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+  ],
+  include: [
+    // Scan your source files for UnoCSS classes and directives
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    // Include your global.css file
+    './src/styles/global.css',
+  ],
+  theme: {
+    fontFamily: {
+      'latin_modern_roman_caps10Ob': ['latin_modern_roman_caps10Ob', 'serif'],
+    },
+  },
+})
