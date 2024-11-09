8bc733bd90b7f8c37855fddc923d4a26ccdfbcc1
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-11-09 08:04:24 -0600

    Animate background

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,15 @@ function App() {
         <Banner/>
 
         {/* Placeholders */}
-        <div className="bg-yellow-400 h-40 flex items-center justify-center">
+        <div className="h-40 flex items-center justify-center">
             <h1 className="text-white text-center">Device Connection</h1>
         </div>
 
-        <div className="bg-yellow-300 h-128 flex items-center justify-center">
+        <div className="h-128 flex items-center justify-center">
             <h1 className="text-white text-center">Graphs and Charts</h1>
         </div>
 
-        <div className="bg-yellow-400 h-128 flex items-center justify-center">
+        <div className="h-128 flex items-center justify-center">
             <h1 className="text-white text-center">AI Integration</h1>
         </div>
 
--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -1,6 +1,6 @@
 function Banner() {
   return (
-        <div className="bg-yellow-500 h-25 flex items-center justify-center">
+        <div className="h-25 flex items-center justify-center">
             <h1 className="text-black text-center text-6xl font-latin_modern_roman_caps10Ob pt-2">Γ</h1>
             <h1 className="text-black text-center text-6xl font-latin_modern_roman_caps10Ob">-sense</h1>
         </div>
