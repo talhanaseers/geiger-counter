c784a513e8403360c2547a160aff6b9eeaa27f1e
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-11-09 08:33:27 -0600

    Change text color in app home

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ import Banner from './components/header/Banner.jsx';
 function App() {
   return (
     <>
-      <div className="position-relative">
+      <div className="position-relative min-w-[400px]">
         <Header/>
         <Banner/>
 
         {/* Placeholders */}
-        <div className="h-40 flex items-center justify-center">
-            <h1 className="text-white text-center">Device Connection</h1>
+        <div className="h-48 flex items-center justify-center">
+            <h1 className="text-black text-center">Device Connection</h1>
         </div>
 
         <div className="h-128 flex items-center justify-center">
-            <h1 className="text-white text-center">Graphs and Charts</h1>
+            <h1 className="text-black text-center">Graphs and Charts</h1>
         </div>
 
         <div className="h-128 flex items-center justify-center">
-            <h1 className="text-white text-center">AI Integration</h1>
+            <h1 className="text-black text-center">AI Integration</h1>
         </div>
 
       </div>
