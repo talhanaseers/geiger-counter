30e8a5912a995c71738f1aea5c0e05f761040b54
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-11-09 03:04:26 -0600

    Update website layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header.jsx';
+import Banner from './components/header/Banner.jsx';
 
 function App() {
   return (
     <>
+      <div className="position-relative">
+        <Header/>
+        <Banner/>
+
+        {/* Placeholders */}
+        <div className="bg-yellow-400 h-40 flex items-center justify-center">
+            <h1 className="text-white text-center">Device Connection</h1>
+        </div>
+
+        <div className="bg-yellow-300 h-128 flex items-center justify-center">
+            <h1 className="text-white text-center">Graphs and Charts</h1>
+        </div>
+
+        <div className="bg-yellow-400 h-128 flex items-center justify-center">
+            <h1 className="text-white text-center">AI Integration</h1>
+        </div>
+
+      </div>
     </>
   )
 }
